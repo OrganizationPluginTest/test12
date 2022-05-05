de99825363a4550a5014c0de116c40d73188c629
anton.mishyn <anton.mishyn>
2022-05-05 19:33:06 +0300

    3334

--- release.sh
+++ release.sh
@@ -2,4 +2,6 @@
 
 read -p 'Input new release version: ' version
 
-mvn -B gitflow:release-start gitflow:release-finish -DreleaseVersion=${version} -DdevelopmentVersion=${version}-SNAPSHOT
+mvn -B gitflow:release-start gitflow:release-finish \
+ -DreleaseVersion=${version} \
+ -DdevelopmentVersion=${version}-SNAPSHOT
